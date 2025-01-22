0190d997fcafca4e8cb2993bd74e09a14001f788
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-01-22 17:58:49 +0530

    making website Responsive

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ function adjustIframeWidthAndHeight() {
   if (screenWidth < 768) {
     setIframeWidthAndHeight("100%", "400");
   } else if (screenWidth < 1024) {
-    setIframeWidthAndHeight("100%", "300");
+    setIframeWidthAndHeight("100%", "600");
   } else {
-    setIframeWidthAndHeight("100%", "400");
+    setIframeWidthAndHeight("100%", "540");
   }
 }
 
 function setIframeWidthAndHeight(ww, hh) {
-  console.log(ww, hh);
+//   console.log(ww, hh);
   const iframeElem = document.getElementById("mainvideofirst");
   iframeElem.setAttribute("width", ww);
   iframeElem.setAttribute("height", hh);
@@ -44,3 +44,34 @@ window.addEventListener("resize", () => {
     adjustIframeWidthAndHeight();
   }
 });
+
+function changeCarouselContent() {
+    //
+}
+
+
+function nextVideoCarousel() {
+    const videoCarousel = document.getElementById("mainvideofirst");
+    const countElem = document.getElementById("currentVideo");
+    vidUrlArray = videoCarousel.getAttribute("src").split("?");
+    vidUrl = vidUrlArray[0];
+    if (vidUrl === "https://play.vidyard.com/nZuhN16mNiJnc7542kqzHW") {
+        vidUrlArray[0] = "https://play.vidyard.com/DRMzgsFprMVo4GXVN2San3";
+        newUrl = vidUrlArray.join("?");
+        videoCarousel.setAttribute("src", newUrl);
+        countElem.innerText = "2";
+    }
+}
+
+function prevVideoCarousel() {
+    const videoCarousel = document.getElementById("mainvideofirst");
+    const countElem = document.getElementById("currentVideo");
+    vidUrlArray = videoCarousel.getAttribute("src").split("?");
+    vidUrl = vidUrlArray[0];
+    if (vidUrl === "https://play.vidyard.com/DRMzgsFprMVo4GXVN2San3") {
+        vidUrlArray[0] = "https://play.vidyard.com/nZuhN16mNiJnc7542kqzHW";
+        newUrl = vidUrlArray.join("?");
+        videoCarousel.setAttribute("src", newUrl);
+        countElem.innerText = "1";
+    }
+}
