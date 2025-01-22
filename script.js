173db0b6c4fec1abf982d4557fc52d4798476c0f
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-01-22 14:33:21 +0530

    Test push

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 function scrollToTheLeft() {
   const elem = document.getElementById("scroll");
   elem.scrollTo({
-    left: elem.scrollLeft - elem.offsetWidth/1.5,
-    behavior: 'smooth'
+    left: elem.scrollLeft - elem.offsetWidth / 1.5,
+    behavior: "smooth",
   });
 }
 function scrollToTheRight() {
   const elem = document.getElementById("scroll");
-    elem.scrollTo({
-        left: elem.scrollLeft + elem.offsetWidth/1.5,
-        behavior: 'smooth'
-    });
+  elem.scrollTo({
+    left: elem.scrollLeft + elem.offsetWidth / 1.5,
+    behavior: "smooth",
+  });
 }
+
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+function adjustIframeWidthAndHeight() {
+    const iframeElem = document.getElementById("mainvideofirst");
+    iframeElem.setAttribute("width", screenWidth);
+    var hh = screenHeight;
+    if (screenWidth < 768) {
+        hh = 500;
+    } else {
+        hh = screenHeight/1.5;
+    }
+    console.log(screenWidth, hh);
+    // console.log("screenHeight: ", screenHeight,hh);
+    iframeElem.setAttribute("height", hh);
+}
+
+adjustIframeWidthAndHeight();
+
+window.addEventListener("resize", () => {
+  if (screenWidth !== window.innerWidth || screenHeight !== window.innerHeight) {
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    adjustIframeWidthAndHeight();
+  }
+});
